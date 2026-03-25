94155904c30c916b98942844ea625f00ff01753e
Woo, Chanje <dnckdwp123@gmail.com>
2026-03-25 10:35:17 +0900

    main 메소드 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,11 +1,8 @@
 package git_test2; 
 
 public class Hello { 
-
-	String message = "안녕하세요"; 
-
-	public void say() { 
+		String message = "안녕하세요"; 
+		public void say() { 
 		System.out.println(message); 
 	} 
-
-}
+} 
--- src/git_test2/main.java
+++
@@ -1,8 +0,0 @@
-package git_test2;
-
-public class main {
-	public static void main(String[] args) { 
-	new Hello().say(); 
-	} 
-
-}
