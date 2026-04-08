764b06717a283e5b151f2e86730bfdd9bf68ada5
Woo, Chanje <dnckdwp123@gmail.com>
2026-04-08 09:56:14 +0900

    main 메소드에 println 추가

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 		String message = "안녕하세요"; 
 		public void say() { 
 		System.out.println(message); 
+	
 	} 
-} 
+}
--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) { 
 		Hello h = new Hello(); 
-		h.say(); 
+		h.say();
+		System.out.println();
 	} 
 } 
